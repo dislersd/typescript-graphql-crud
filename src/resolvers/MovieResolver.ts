1bdd2800a13dece259dabed824efc5cdb80fff88
dislersd <dislersd@gmail.com>
2020-05-11 20:36:44 -0700

    added type decorators, can create and read movies

--- src/resolvers/MovieResolver.ts
+++ src/resolvers/MovieResolver.ts
@@ -1,13 +1,30 @@
-import { Resolver, Mutation, Arg, Int } from "type-graphql";
-
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  Int,
+  Query,
+  InputType,
+  Field,
+} from "type-graphql";
+import { Movie } from "../entity/Movie";
+@InputType()
+class MovieInput {
+  @Field()
+  title: string;
+  @Field(() => Int)
+  minutes: number;
+}
 @Resolver()
 export class MovieResolver {
-  @Mutation(() => Boolean)
-  createMovie(
-    @Arg("title", () => String) title: string,
-    @Arg("minutes", () => Int) minutes: number
-    ) {
-    console.log(title);
-    return true;
+  @Mutation(() => Movie)
+  async createMovie(@Arg("options", () => MovieInput) options: MovieInput) {
+    const movie = await Movie.create(options).save();
+    return movie;
+  }
+
+  @Query(() => [Movie])
+  movies() {
+    return Movie.find();
   }
 }
